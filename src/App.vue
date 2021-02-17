34c9ada1459f7f327851a8a9ff00e21733c6bbb4
Simon <silvestresimon@icloud.com>
2021-02-17 18:41:24 +0100

    margin

--- src/App.vue
+++ src/App.vue
@@ -288,7 +288,7 @@ export default {
 
   @media screen and (max-width: 540px) {
     .card {
-      margin: 0 25px 100px 0;
+      margin: 0 25px 100px 25px;
       .top {
         padding: 60px 20px 35px 20px;
       }
